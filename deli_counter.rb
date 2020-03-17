f6f7c0cfcbf86e159bc0f84a882926119db3716f
Jimmy Le <jimmyle414@gmail.com>
2020-03-17 09:06:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,3 +12,24 @@
     
 #   end
 # end
+
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  position = katz_deli.index(name)
+  puts"Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
+
+  return name, position
+end
+
+  def line(katz_deli)
+ if katz_deli.empty? == false
+   katz_deli.each_with_index do|value,index|
+
+     puts "The line is currently: #{index+1}. #{value}"
+ end
+     elsif katz_deli.empty? == true
+  puts "The line is currently empty."
+ end
+end
