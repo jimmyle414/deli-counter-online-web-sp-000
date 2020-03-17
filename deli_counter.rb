85f631c79a87b34f6bb7d7ee5da542b73ca11ab6
Jimmy Le <jimmyle414@gmail.com>
2020-03-17 09:12:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 # Write your code here.
 katz_deli = []
 
-def line(place)
-  if place.length == 0
+def line(katz_deli)
+  if katz_deli.length == 0
     puts "The line is currently empty."
   else
     message = "The line is currently:"
     
-    place.each.with_index do |name, index|
+    katz_deli.each.with_index do |name, index|
       message += " #{index.to_i+1}. #{name}"
       
     end
@@ -16,4 +16,4 @@ def line(place)
   end
 end
 
-def take_a_number
+# def take_a_number()
