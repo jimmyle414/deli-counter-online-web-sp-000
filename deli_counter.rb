79275c2162b95fe568a61f5bab504503e2bdc46d
Jimmy Le <jimmyle414@gmail.com>
2020-03-17 09:54:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,26 +1,6 @@
 # Write your code here.
 katz_deli = []
 
-# def line(katz_deli)
-#   if katz_deli.length == 0
-#     puts "The line is currently empty."
-#   else
-#     message = "The line is currently:"
-    
-#     katz_deli.each.with_index do |name, index|
-#       message += " #{index.to_i+1}. #{name}"
-      
-#     end
-#     puts "#{message}"
-    
-#   end
-# end
-
-# def take_a_number(katz_deli, name)
-#   katz_deli.push(name)
-#   puts "Welcome, #{name}. You are number #{katz_deli.length} in line"
-# end
-
 def line(katz_deli)
   line_positions = [];
   if katz_deli.empty?
